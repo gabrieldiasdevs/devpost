4c00a6ae84c8b3b982e62d36a5e1a1d6be613af9
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-04-29 09:19:19 -0300

    página search

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,57 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import Feather from 'react-native-vector-icons/Feather'
+import firestore from '@react-native-firebase/firestore'
+
+import { 
+  Container, 
+  AreaInput, 
+  Input,
+  List
+} from './styles'
 
 export default function Search(){
+  const [input, setInput] = useState('')
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+
+    if(input === '' || input === undefined){
+      setUsers([])
+      return
+    }
+
+    const subscriber = firestore().collection('users')
+    .where('nome', '>=', input)
+    .where('nome', '<=', input + "/uf8ff")
+    .onSnapshot( snapshot => {
+      const listUsers = []
+
+      snapshot.forEach(doc => {
+        listUsers.push({
+          ...doc.data(),
+          id: doc.id,
+        })
+      })
+
+      setUsers(listUsers)
+
+    })
+
+    return () => subscriber()
+
+  }, [input])
+
   return(
-    <View>
-      <Text>SEARCH</Text>
-    </View>
+    <Container>
+      <AreaInput>
+        <Feather name='search' size={20} color='#E52246'/>
+        <Input
+          placeholder='Procurando alguem?'
+          value={input}
+          onChangeText={ (text) => setInput(text) }
+          placeholderTextColor='#353840'
+        />
+      </AreaInput>
+    </Container>
   )
 }
